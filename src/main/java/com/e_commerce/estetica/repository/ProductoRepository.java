d0dd26627dbeed7d073f31efa6aff8076fca7773
darkepineda <darkelyspineda@gmail.com>
2026-09-03 13:48:08 -0300

    Resolver todos los conflictos de merge x2

--- src/main/java/com/e_commerce/estetica/repository/ProductoRepository.java
+++ src/main/java/com/e_commerce/estetica/repository/ProductoRepository.java
@@ -2,8 +2,6 @@ package com.e_commerce.estetica.repository;
 
 import com.e_commerce.estetica.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
 }
--- src/main/java/com/e_commerce/estetica/repository/UsuarioRepository.java
+++ src/main/java/com/e_commerce/estetica/repository/UsuarioRepository.java
@@ -2,9 +2,7 @@ package com.e_commerce.estetica.repository;
 
 import com.e_commerce.estetica.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     
 }
