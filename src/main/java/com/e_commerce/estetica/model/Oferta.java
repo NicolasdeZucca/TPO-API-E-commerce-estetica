33e01fad6201d6242230bb9e93bf85779b7764a9
NicolasdeZucca <zuccarellonj@gmail.com>
2026-09-04 16:20:28 -0300

    Modelo "Oferta" completo. Cambio nombre "precioDescuento" por "porcentajeDescuento".

--- src/main/java/com/e_commerce/estetica/model/Oferta.java
+++ src/main/java/com/e_commerce/estetica/model/Oferta.java
@@ -1,24 +1,44 @@
 package com.e_commerce.estetica.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Oferta {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String titulo;
-    private String descripcion;
-    private Double precioDescuento;
 
-    // Muchas ofertas pueden aplicar a un producto
-    @ManyToOne
-    @JoinColumn(name = "producto_id")
+    @Column(nullable = false)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private Double porcentajeDescuento;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaInicio;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaFin;
+
+    @Column(nullable = false)
+    private Boolean activa = false;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "producto_id", nullable = false)
     private Producto producto;
 }
