518fe0f6864c24cc1b2854362f8eb9b0819a73a5
NicolasdeZucca <zuccarellonj@gmail.com>
2026-09-04 16:21:27 -0300

    Agrego atributos faltantes a "Producto" junto con sus relaciones. Modelo "Producto" completo.

--- src/main/java/com/e_commerce/estetica/model/Producto.java
+++ src/main/java/com/e_commerce/estetica/model/Producto.java
@@ -2,25 +2,50 @@ package com.e_commerce.estetica.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nombre;
+
+    @Column(nullable = false)
+    private String descripcion;
+
+    @Column(nullable = false)
     private String marca;
+
+
+    @Column (nullable = false)
     private Double precio;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
+
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductoImagen> imagenes = new ArrayList<>();
 
     // Un producto puede tener muchas ofertas
     @OneToMany(mappedBy = "producto")
     @JsonIgnore // Evita loop infinito en JSON
-    private List<Oferta> ofertas;
+    private List<Oferta> ofertas = new ArrayList<>();
 }
