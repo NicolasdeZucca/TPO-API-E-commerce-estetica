c9894c8e38a7174b7ad45cb26d1557d111794b54
NicolasdeZucca <zuccarellonj@gmail.com>
2026-09-04 20:01:01 -0300

    Listo uachin !

--- src/main/java/com/e_commerce/estetica/dto/RegisterRequest.java
+++ src/main/java/com/e_commerce/estetica/dto/RegisterRequest.java
@@ -8,9 +8,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class RegisterRequest {
-    @NotBlank(message = "El nombre de usuario es obligatorio")
-    private String nombreUsuario;
-
     @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
 
