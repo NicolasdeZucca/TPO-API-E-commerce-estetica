25fcf92ca982eaf1e1b2b49e4b07b633a3b8eb8b
NicolasdeZucca <zuccarellonj@gmail.com>
2026-09-04 16:23:42 -0300

    Relacion entre Usuario y Carrito

--- src/main/java/com/e_commerce/estetica/model/Usuario.java
+++ src/main/java/com/e_commerce/estetica/model/Usuario.java
@@ -1,7 +1,9 @@
 package com.e_commerce.estetica.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,13 +15,18 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nombre;
+
+    @Column(nullable = false)
     private String apellido;
 
     @Column(unique = true, nullable = false)
@@ -32,8 +39,13 @@ public class Usuario implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role rol;
 
-    // --- Métodos de UserDetails (los necesita Spring Security) ---
+    @OneToOne(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private Carrito carrito;
 
+
+
+
+    // --- Métodos de UserDetails (los necesita Spring Security) ---
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(rol.name()));
