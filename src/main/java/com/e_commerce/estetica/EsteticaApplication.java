5c91fe703de991a36560e71d309c10d4296b354b
dpata <dpata@uade.edu.ar>
2026-09-15 15:42:03 -0300

    Mensaje de inicio de la API

--- src/main/java/com/e_commerce/estetica/EsteticaApplication.java
+++ src/main/java/com/e_commerce/estetica/EsteticaApplication.java
@@ -1,16 +1,23 @@
 package com.e_commerce.estetica;
 
 import com.e_commerce.estetica.service.CategoriaService;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class EsteticaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EsteticaApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	void mostrarMensajeDeInicio() {
+		System.out.println("API de Estetica escuchando en http://localhost:8080");
 	}
 
 	@Bean
